cd0cc32e85ee5be38afcb91e5e497e1ea9324187
Mike Vaughan <dvaugha1@gmail.com>
2015-02-01 19:32:19 -0500

    HW so far

--- lib/notepasser.rb
+++ lib/notepasser.rb
@@ -6,10 +6,49 @@ require "pry"
 Camping.goes :Notepasser
 
 module Notepasser
+
 end
 
 module Notepasser::Models
+  class User < Base
+    has_many :messages
+  end
+
+  class Message < Base
+    belongs_to :user
+  end
 end
 
 module Notepasser::Controllers
+  class NewuserController < R '/users'
+    def create_user
+    username = Notepasser::Models::User.create
+      [:user, :user_id].each do |t|
+        username[t] = @input[t]
+      end
+    username.save
+    {create_user => username}.to_json
+    end
+  end
+
+  class UserController < R '/users/(\d+)'
+    def get(user_id)
+      user = Notepasser::Models::User.find(user_id)
+    end
+
+    def delete(user_id)
+      user = Notepasser::Models::User.destroy(user_id)
+    end
+  end
+
+  class NewmessageController < R '/msg/new'
+    def create_msg
+    msg = Notpasser::Models::Message.create
+    [:message, :message_id, :user_id, :receive_id].each do |m|
+      msg[m] = @input[m]
+     end
+     msg.save
+     {create_msg => msg}.to_json
+  end
+
 end
